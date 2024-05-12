1b2272e2f0ab16a5382e8f22b2db9d83efac5b3d
wanglingan <wanglingan@mysteel.com>
2024-05-13 00:36:31 +0800

    ✨ feat: 更新vue 模板

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="blanc-nova-vue3-template">
+    <div ref="containerRef" class="three-container" />
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.welcome {
+  width: 100%;
+  height: 100%;
+}
+</style>
